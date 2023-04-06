7c09e14fe09ac1f3845e3c8861477e523da73571
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-07 02:34:48 +0530

    created basic server

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server Listening on PORT::${PORT}...`);
+});
+
+/* express is basically just a fancy listener function/middleware that we add on top our built in node http server */
+
+/* any middleware or route handlers attached to app object will respond to requests coming in to our server */
+
+/* using built in http server allows us to not only respond to http requests but also to other types of connections for eg. web sockets for real time communication  */
