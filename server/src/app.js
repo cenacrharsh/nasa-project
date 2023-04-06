b36abffd7519c1a3748e5ae5f26e5242da93b74d
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-04-07 03:07:53 +0530

    fixed cors error

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
 //! Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
 module.exports = app;
+
+/* ORIGIN for our API & Frontend is different as origin includes Protocol (http://), URL Hostname (localhost) and PORT number (3000) & our server & client are running on different ports */
